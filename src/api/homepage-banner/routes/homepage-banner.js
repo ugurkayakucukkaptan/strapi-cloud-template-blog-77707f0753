9b2ab493119a43f36b8ad7170628d7ac0588ca90
Ugur Kaya Kucukkaptan <ugurkaya97@hotmail.com>
2025-05-27 16:49:27 +0300

    added homepage banner collection

---
+++ src/api/homepage-banner/routes/homepage-banner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * homepage-banner router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::homepage-banner.homepage-banner');
