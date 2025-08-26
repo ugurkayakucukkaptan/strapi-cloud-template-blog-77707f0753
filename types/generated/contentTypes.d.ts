a9ea9551c0bda1ea409154e9347dbb48675bd825
Ugur Kaya Kucukkaptan <ugurkaya97@hotmail.com>
2025-08-26 21:43:21 +0300

    added draft to aboutus

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -378,7 +378,7 @@ export interface ApiAboutAbout extends Struct.SingleTypeSchema {
     singularName: 'about';
   };
   options: {
-    draftAndPublish: false;
+    draftAndPublish: true;
   };
   pluginOptions: {
     i18n: {
